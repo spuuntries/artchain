19fa685bfb910cc7139ba92ed483866ea5ab4595
spuuntries <kek@spuun.art>
2024-03-23 22:22:45 +0700

    revert!: Swap over to stero

--- index.mjs
+++ index.mjs
@@ -2,17 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import fs from "node:fs";
-const wordlist = JSON.parse(fs.readFileSync("./wordlist.json")).list;
+const wordlist = JSON.parse(fs.readFileSync("./wordlist.json").toString()).list;
 
 import * as Discord from "discord.js";
-import { createLibp2p } from "libp2p";
-import { tcp } from "@libp2p/tcp";
-import { webSockets } from "@libp2p/websockets";
-import { noise } from "@chainsafe/libp2p-noise";
-import { mplex } from "@libp2p/mplex";
-import { multiaddr } from "@multiformats/multiaddr";
-import { ping } from "@libp2p/ping";
-import crypto from "node:crypto";
+import { joinRoom } from "trystero";
 
 const procenv = process.env,
   client = new Discord.Client({ intents: ["MessageContent", "GuildMessages"] });
@@ -50,22 +43,10 @@ async function getWord() {
   return wordlist[await generateRandom(0, wordlist.length - 1)];
 }
 
-createLibp2p({
-  addresses: {
-    listen: ["/ip4/127.0.0.1/tcp/0"],
-  },
-  transports: [tcp(), webSockets()],
-  connectionEncryption: [noise()],
-  streamMuxers: [mplex()],
-  services: { ping: ping() },
-}).then(async (node) => {
-  await node.start().then(() => {
-    console.log("listening on addresses:");
-    node.getMultiaddrs().forEach((addr) => {
-      console.log(addr.toString());
-    });
-  });
-});
+const room = joinRoom({ appId: "art_chain_rocks" }, "primary");
+
+room.onPeerJoin((peerId) => console.log(`${peerId} joined`));
+room.onPeerLeave((peerId) => console.log(`${peerId} left`));
 
 // setInterval(() => {
 //   if (Date.now() % 3 != 0) return; // Do aggregation only on every third
